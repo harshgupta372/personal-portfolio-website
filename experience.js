c7f6888ebe38f6110cf5dcdc978266ecd66580c6
harshgupta372 <167362291+harshgupta372@users.noreply.github.com>
2024-10-02 00:34:33 +0530

    Add files via upload

---
+++ experience.js
@@ -0,0 +1,7 @@
+ 
+ 
+  const button = document.getElementById('myButton');
+  button.addEventListener('click', () => {
+    window.location.href = 'https://www.github.com';  // Replace with your target URL
+  });
+ 
